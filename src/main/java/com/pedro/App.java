c329dc111bef9a0e15cd153c0f911a9461bbf060
Pedro Rivas <122261595+PedroRiRe@users.noreply.github.com>
2023-01-19 09:36:29 +0100

    Update App.java

--- src/main/java/com/pedro/App.java
+++ src/main/java/com/pedro/App.java
@@ -2,6 +2,7 @@ package com.pedro;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Este cambio está hecho desde Github")
+        System.out.println("Este cambio está hecho desde Github");
+        System.out.println("provocar un conflicto");
     }
 }
